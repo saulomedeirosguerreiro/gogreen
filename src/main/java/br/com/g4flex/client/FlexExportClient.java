98286598887025ce969dd182d5b9558a07a0e10c
Saulo Medeiros <saulo.medeiros@g4flex.com.br>
2019-10-09 09:59:23 -0300

    correcao bug relacionado a  geracao de excel

--- src/main/java/br/com/g4flex/client/FlexExportClient.java
+++ src/main/java/br/com/g4flex/client/FlexExportClient.java
@@ -1,6 +1,7 @@
 package br.com.g4flex.client;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -10,13 +11,14 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 public class FlexExportClient {
 
 	private final static String BASE_URL = "http://10.8.60.70:3330";
 //	private final static String BASE_URL = "http://localhost:3333";
-	
+
 	private static HttpURLConnection init(String url) throws IOException {
 		URL requestUrl = new URL(url);
 		HttpURLConnection urlConnection = (HttpURLConnection) requestUrl.openConnection();
@@ -39,24 +41,41 @@ public class FlexExportClient {
 		HttpURLConnection myUrlConnection = init(BASE_URL + "/exports/excel");
 
 		sendData(myUrlConnection, json);
-		receiveData(myUrlConnection, response, "application/xlsx; charset=utf-8", filename);
+		receiveData(myUrlConnection, response, "application/vnd.ms-excel; charset=utf-8", filename);
 	}
 
 	private static void receiveData(HttpURLConnection con, HttpServletResponse response, String contentType,
 			String filename) throws IOException {
-		InputStream in = new BufferedInputStream(con.getInputStream());
-		byte dataBuffer[] = new byte[40000000];
-		int bytesRead;
-		while ((bytesRead = in.read(dataBuffer, 0, 40000000)) != -1) {
-			response.getOutputStream().write(dataBuffer, 0, bytesRead);
+		ServletOutputStream servletOutputStream = null;
+		try {
+			servletOutputStream = response.getOutputStream();
+			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+			InputStream is = con.getInputStream();
+			int byteRead;
+			byte[] data = new byte[con.getContentLength() < 1 ? 2048 : con.getContentLength()];
+
+			while ((byteRead = is.read(data, 0, data.length)) != -1) {
+				buffer.write(data, 0, byteRead);
+			}
+
+			response.addHeader("Content-Disposition", "attachment; filename=" + filename);
+
+			byte[] bytes = buffer.toByteArray();
+			response.setContentType(contentType);
+			response.setContentLength(bytes.length);
+			buffer.close();
+
+			servletOutputStream = response.getOutputStream();
+			servletOutputStream.write(bytes, 0, bytes.length);
+			servletOutputStream.flush();
+			servletOutputStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeQuietly(servletOutputStream);
 		}
-		response.setContentLength(bytesRead);
-		response.setContentType(contentType);
-		String headerKey = "Content-Disposition";
-		String headerValue = String.format("attachment; filename=\"" + filename + "\"");
-		response.setHeader(headerKey, headerValue);
-		closeQuietly(in);
-		
+
 	}
 
 	private static void sendData(HttpURLConnection con, String data) throws IOException {
